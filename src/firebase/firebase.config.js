5003d7678427c7fdd2cdfdad7e7a4aa8657d93e6
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-10-25 12:37:56 +0600

    Git setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAxJtuZtxS9RJOA4wN6L4Uo1loYKverXeQ",
+    authDomain: "bizon-learning.firebaseapp.com",
+    projectId: "bizon-learning",
+    storageBucket: "bizon-learning.appspot.com",
+    messagingSenderId: "1000312709155",
+    appId: "1:1000312709155:web:b064b7fe0328b5d3571196",
+    measurementId: "G-BP0VFDLXKY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default app;
